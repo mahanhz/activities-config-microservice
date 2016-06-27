47ef33db6dac806bb57359ebbe5079b9f8943f74
Mahan Hashemizadeh <mahan16@hotmail.com>
2016-06-27 21:15:51 +0200

    Initial commit

---
+++ src/main/java/com/amhzing/ActivitiesConfigApplication.java
@@ -0,0 +1,14 @@
+package com.amhzing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ActivitiesConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesConfigApplication.class, args);
+	}
+}
