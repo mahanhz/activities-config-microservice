cfe99f867f1e248f22d13e4e553354ec0b2db1c8
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-07-03 21:00:27 +0200

    rc 2

--- scripts/release/activities_config_release.sh
+++ scripts/release/activities_config_release.sh
@@ -4,7 +4,7 @@ echo "Passed in arguments are: $1, $2"
 version=$1
 semanticVersionSegment=$2
 
-echo "current version=$version"
+echo "current version=$version, semanticVersionSegment=$semanticVersionSegment"
 
 major=`echo $version | cut -d. -f1`
 minor=`echo $version | cut -d. -f2`
@@ -12,17 +12,17 @@ patch=`echo $version | cut -d. -f3`
 
 echo "major=$major, minor=$minor, patch=$patch"
 
-if [ "$semanticVersionSegment" == "patch" ]; then
+if [ $semanticVersionSegment = "patch" ]; then
 	((patch++))
-elif [ "$semanticVersionSegment" == "minor" ]; then
+elif [ $semanticVersionSegment = "minor" ]; then
 	((minor++))
 	patch=0
-elif [ "$semanticVersionSegment" == "major" ]; then
+elif [ $semanticVersionSegment = "major" ]; then
 	((major++))
 	minor=0
 	patch=0
 else
-	echo "Invalid release type: $semanticVersionSegment"
+	echo "Invalid semantic version segment: $semanticVersionSegment"
 fi
 
 version=$major.$minor.$patch
